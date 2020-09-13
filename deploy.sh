daf740bf10748b7f640f6b801fc6748932c9ac1c
cdbbnny <cdbbnnycode@users.noreply.github.com>
2020-09-13 16:32:37 -0500

    python --version

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "GitHub actions test"
 echo "Running on $(uname -a)"
-echo "Python version: $(python -v)"
+echo "Python version: $(python --version)"
 echo "Where am I: $(realpath .)"
 echo "Home: $HOME"
 ls
