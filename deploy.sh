f82bf27a04e945f7702d6854bbd1cf5f82b75185
cdbbnny <cdbbnnycode@users.noreply.github.com>
2020-09-13 16:47:27 -0500

    Test with named run

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "GitHub actions test"
 echo "Running on $(uname -a)"
-echo "Python version: $(python --version)"
+echo "Python version: $(python3 --version)"
 echo "Where am I: $(realpath .)"
 echo "Home: $HOME"
 ls
